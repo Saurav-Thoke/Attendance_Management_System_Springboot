663d5fbea6191240731123e6ea30e4c69fdf5126
Saurav Thoke <sauravthoke28@gmail.com>
2024-11-06 21:34:45 +0530

    First Commit Trial

---
+++ src/test/java/com/saurav/Attendance/Management/System/AttendanceManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurav.Attendance.Management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
